111f784026bc06dce25e3fd1b001b38c74162c87
João Quessada <joaoquessada4@gmail.com>
2023-03-20 09:58:28 -0300

    definindo os atributos das classes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sqlite3
 
 Class Produto:
-	continue
+	def __init__(self, codigo_produto, preco_compra, preco_venda, quantidade, categoria):
+		self.codigo_produto = codigo_produto
+		self.preco_compra = preco_compra
+		self.preco_venda = preco_venda
+		self.quantidade = quantidade
+		self.categoria = categoria
 
 Class Estoque:
-	continue
+	sef __init__(self):
+	self.conexao = sqlite3.connect('estoque.db')
