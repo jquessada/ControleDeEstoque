707e797fe7b821571e5b5b954cc2f44681af43f9
João Quessada <joaoquessada4@gmail.com>
2023-03-21 19:45:05 -0300

    Corrigindo a identação

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 import sqlite3
 
 class Produto:
-	def __init__(self, codigo_produto, preco_compra, preco_venda, quantidade, categoria):
-		self.codigo_produto = codigo_produto
-		self.preco_compra = preco_compra
-		self.preco_venda = preco_venda
-		self.quantidade = quantidade
-		self.categoria = categoria
+    def __init__(self, codigo_produto, preco_compra, preco_venda, quantidade, categoria):
+        self.codigo_produto = codigo_produto
+        self.preco_compra = preco_compra
+        self.preco_venda = preco_venda
+        self.quantidade = quantidade
+        self.categoria = categoria
 
 class Estoque:
-	def __init__(self):
-		self.connection = sqlite3.connect('estoque.db')
-		self.cursor = self.connection.cursor()
+    def __init__(self):
+        self.conexao = sqlite3.connect('estoque.db')
+        self.cursor = self.conexao.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS produtos (codigo_produto INTEGER, preco_compra REAL, preco_venda REAL, quantidade INTEGER, categoria TEXT)')
-        self.connection.commit()
+        self.conexao.commit()
 
-	def adicionar_produto(self, produto):
-		self.cursor.execute('INSERT INTO produtos VALUES (?, ?, ?, ?, ?)', (produto.codigo_produto, produto.preco_compra, produto.preco_venda, produto.quantidade, produto.categoria))
-		self.connection.commit()
+    def adicionar_produto(self, produto):
+        self.cursor.execute('INSERT INTO produtos VALUES (?, ?, ?, ?, ?)', (produto.codigo_produto, produto.preco_compra, produto.preco_venda, produto.quantidade, produto.categoria))
+        self.conexao.commit()
 
-	def remover_produto(self, codigo_produto):
-		self.cursor.execute('DELETE FROM produtos WHERE codigo_produto = ?', (codigo_produto,))
-		self.connection.commit()
+    def remover_produto(self, codigo_produto):
+        self.cursor.execute('DELETE FROM produtos WHERE codigo_produto = ?', (codigo_produto,))
+        self.conexao.commit()
 
-	def consultar_produto(self, codigo_produto):
-		self.cursor.execute('SELECT * FROM produtos WHERE codigo_produto = ?', (codigo_produto,))
-		produto = self.cursor.fetchone()
-		if produto:
-			return Produto(*produto)
-		else:
-			return None
+    def consultar_produto(self, codigo_produto):
+        self.cursor.execute('SELECT * FROM produtos WHERE codigo_produto = ?', (codigo_produto,))
+        produto = self.cursor.fetchone()
+        if produto:
+            return Produto(*produto)
+        else:
+            return None
 
-	def listar_produtos(self):
-		self.cursor.execute('SELECT * FROM  produtos')
-		produtos = []
-		for produto in self.cursor.fetchall():
-			produtos.append(Produto(*produto))
-		return produtos
+    def listar_produtos(self):
+        self.cursor.execute('SELECT * FROM produtos')
+        produtos = []
+        for produto in self.cursor.fetchall():
+            produtos.append(Produto(*produto))
+        return produtos
 
-	def sair(self):
-		self.connection.close()
+    def sair(self):
+        self.conexao.close()
